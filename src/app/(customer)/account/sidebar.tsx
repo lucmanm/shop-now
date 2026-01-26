36381b6ddab60d33372c2bc6ed434ab6f29117c5
lucmanm@icloud.com <lucmanm@icloud.com>
2026-01-27 01:44:00 +0300

    fixing problem showing sidbar

--- src/app/(customer)/account/sidebar.tsx
+++ src/app/(customer)/account/sidebar.tsx
@@ -28,7 +28,7 @@ export function Nav({ links, isCollapsed }: NavProps) {
         data-collapsed={isCollapsed}
         className="group flex flex-col gap-4 rounded-md border px-1 py-2 data-[collapsed=true]:py-2 capitalize"
       >
-        <nav className="grid gap-1 px-1 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+        <nav className="grid gap-1 px-1 group-data-[collapsed=true]:justify-center group-data-[collapsed=true]:px-2">
           {links.map((link, index) =>
             isCollapsed ? (
               <Tooltip key={index} delayDuration={0}>
