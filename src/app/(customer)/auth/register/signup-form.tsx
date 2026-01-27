82dd41be0a7dfed1dcd93fc965f3cc4e5061abd8
lucmanm@icloud.com <lucmanm@icloud.com>
2026-01-28 00:28:33 +0300

    better auth additional config

--- src/app/(customer)/auth/register/signup-form.tsx
+++ src/app/(customer)/auth/register/signup-form.tsx
@@ -12,8 +12,9 @@ import {
 import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Controller, useForm } from "react-hook-form"
-import { toast } from "sonner"
 import * as z from "zod"
+import { signUp } from "@/lib/auth-client"
+import { toast } from "sonner"
 
 const formSchema = z.object({
   fullname: z
@@ -44,22 +45,22 @@ export function SignupForm({
     },
   })
 
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    toast("You submitted the following values:", {
-      description: (
-        <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
-          <code>{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-      position: "bottom-right",
-      classNames: {
-        content: "flex flex-col gap-2",
-      },
-      style: {
-        "--border-radius": "calc(var(--radius)  + 4px)",
-      } as React.CSSProperties,
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+   try {
+
+    const result = await signUp.email({
+      name: data.fullname,
+      email: data.email,
+      password: data.password,
     })
-    console.log(data)
+
+    if(result.error){
+      toast.error(result.error.message)
+      return
+    }
+   } catch (error) {
+    toast.error("Something went wrong. Please try again.")
+   }
   }
 
   return (
--- src/app/(customer)/layout.tsx
+++ src/app/(customer)/layout.tsx
@@ -7,6 +7,7 @@ export default function DashboardLayout({
     <div className="container mx-auto min-w-full flex-1 overflow-hidden ">
       <div className="flex min-h-screen flex-col items-center justify-between ">
         {children}
+
       </div>
     </div>
   );
