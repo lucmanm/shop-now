961b61a09336e117dc2c54266d35c76f777a5e41
lucmanm@icloud.com <lucmanm@icloud.com>
2026-01-28 00:05:02 +0300

    better auth config update

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,3 +1,7 @@
 import { createAuthClient } from 'better-auth/react'
 
-export const { signIn, signUp, signOut, useSession } = createAuthClient()
+export const authClient = createAuthClient({
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: process.env.BETTER_AUTH_URL,
+})
+export const { signIn, signUp, signOut, useSession, resetPassword } = createAuthClient()
